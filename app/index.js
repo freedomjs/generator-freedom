d6f6babd8860d3ab41fe76c4d579b769f8783330
soycode <soycode@users.noreply.github.com>
2015-02-23 14:23:09 -0800

    gruntfile, git, bootstrap, more user interaction

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 'use strict';
 var generators = require('yeoman-generator');
-var appname, freedomsource, freedomtype;
+var appname, freedomsource, freedomtype, git, gruntfile, bootstrap;
+var freedomchoices = ['freedom', 'freedom-for-firefox', 'freedom-for-chrome'];
 
 module.exports = generators.Base.extend({
-  prompting: function () {
+  // Using multiple prompts because later ones depend on earlier ones
+  prompt1: function () {
     var done = this.async();
     this.prompt([
       {
@@ -19,20 +21,51 @@ module.exports = generators.Base.extend({
         choices : [ 'npm', 'bower', 'freedomjs.org' ]
       },
       {
-        type    : 'list',
-        name    : 'freedomtype',
-        message : 'Which flavor of freedom.js would you like?',
-        choices : [ 'freedom', 'freedom-for-firefox',
-                    'freedom-for-chrome', 'freedom-for-node' ]
+        type    : 'confirm',
+        name    : 'git',
+        message : "Would you like to to use git?",
+        default : true
+      },
+      {
+        type    : 'confirm',
+        name    : 'gruntfile',
+        message : "Would you like to make a Gruntfile (requires npm)?",
+        default : true
+      },
+      {
+        type    : 'confirm',
+        name    : 'bootstrap',
+        message : "Would you like to use Bootstrap (requires bower)?",
+        default : false
       }
     ], function (answers) {
          appname = answers.name;
          freedomsource = answers.freedomsource;
+         if (freedomsource !== 'freedomjs.org') {
+           freedomchoices.push('freedom-for-node');
+         }
+         git = answers.git;
+         gruntfile = answers.gruntfile;
+         bootstrap = answers.bootstrap;
+         done();
+       }.bind(this));
+  },
+  prompt2: function () {
+    var done = this.async();
+    this.prompt([
+      {
+        type    : 'list',
+        name    : 'freedomtype',
+        message : 'Which flavor of freedom.js would you like ' +
+          '(if uncertain, choose "freedom")?',
+        choices : freedomchoices
+      }
+    ], function (answers) {
          freedomtype = answers.freedomtype;
          done();
        }.bind(this));
   },
-  getfreedom: function() {
+  getfreedom: function () {
     if (freedomsource === 'npm') {
       this.npmInstall([freedomtype]);
     } else if (freedomsource === 'bower') {
@@ -40,12 +73,58 @@ module.exports = generators.Base.extend({
       // but I think the right fix is to publish the others there
       this.bowerInstall([freedomtype]);
     } else if (freedomsource === 'freedomjs.org') {
-      freedomtype += '.js';
-      if (freedomtype === 'freedom-for-firefox.js') {
-        freedomtype += 'm';  // ff addon uses .jsm files
+      var freedomurl = 'http://www.freedomjs.org/dist/latest/' + freedomtype + '.js';
+      if (freedomtype === 'freedom-for-firefox') {
+        freedomurl += 'm';  // ff addon uses .jsm files
       }
-      this.fetch('http://www.freedomjs.org/dist/latest/' + freedomtype, './',
+      this.fetch(freedomurl, 'deps/',
                  function (err) { if (err) { console.error(err); } } );
+    }
+  },
+  makemanifest: function () {
+    this.fs.copyTpl(
+      this.templatePath('manifest.json'),
+      this.destinationPath('manifest.json'),
+      { appname: appname }
+    );
+  },
+  getbootstrap: function() {
+    if (bootstrap) {
+      this.composeWith('backbone:route', {}, {
+        local: require.resolve('generator-bootstrap')
+      });
+    }
+  },
+  setupgrunt: function () {
+    if (gruntfile) {
+      this.npmInstall(['grunt-contrib-clean', 'grunt-contrib-connect',
+                       'grunt-contrib-copy', 'grunt-contrib-jshint']);
+      var freedompath;
+      if (freedomsource === 'npm') {
+        freedompath = 'require.resolve(\'' + freedomtype + '\')';
+      } else if (freedomsource === 'bower') {
+        // TODO figure proper way to copy bower-installed freedom
+      } else if (freedomsource === 'freedomjs.org') {
+        freedompath = '\'deps/' + freedomtype + '.js';
+        if (freedomtype === 'freedom-for-firefox') {
+          freedompath += 'm';  // ff addon uses .jsm files
+        }
+        freedompath += '\'';
+      }
+      this.fs.copyTpl(
+        this.templatePath('Gruntfile.js'),
+        this.destinationPath('Gruntfile.js'),
+        { appname: appname, freedompath: freedompath }
+      );
+    }
+  },
+  setupgit: function () {
+    if (git) {
+      this.spawnCommand('git', ['init']);
+      this.fs.copyTpl(
+        this.templatePath('.gitignore'),
+        this.destinationPath('.gitignore')
+      );
     }
   }
 });
